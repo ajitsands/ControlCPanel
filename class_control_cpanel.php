7690117af79093ffdfecf7496e1e6c6e5d86b4d2
Ajit Kumar <ajitsands@gmail.com>
2024-05-12 00:34:48 +0530

    AddNewSubdomain-1205-2024

--- class_control_cpanel.php
+++ class_control_cpanel.php
@@ -11,14 +11,53 @@ class ClassControlCpanel
         $this->domain = DOMAIN;
         $this->directory = DIRECTORY;
 
-        echo $this->directory;
+    }
+
+    public function AddNewSubdomain()
+    {
+        $query_params = array(
+            'cpanel_jsonapi_module' => 'SubDomain',
+            'cpanel_jsonapi_func' => 'addsubdomain',
+            'cpanel_jsonapi_version' => 2,
+            'domain' => $this->subdomain,
+            'rootdomain' => $this->domain,
+            'dir' => $this->directory
+        );
+        
+        $query = "https://$this->domain:2083/json-api/cpanel?" . http_build_query($query_params);
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_URL, $query);
+        curl_setopt($curl, CURLOPT_USERPWD, "$this->cpanel_username:$this->cpanel_password");
+        
+        $result = curl_exec($curl);
+        if ($result === false) {
+            error_log("cURL error: " . curl_error($curl));
+        } else {
+            echo $result;
+        }
+        
+        curl_close($curl);
+        
+        $data = json_decode($json_data, true);
+        
+        // Access the value of 'reason' under 'data' array
+        $reason = $data['cpanelresult']['data'][0]['reason'];
+        
+        // Access the value of 'module'
+        $module = $data['cpanelresult']['module'];
+        
+        // Output the values
+        echo "Reason: $reason\n";
+        echo "Module: $module\n";
     }
     
     
     
     
     public function __destruct() {
-        echo "This is for Testing";
+       // echo "This is for Testing";
     }
 
 }
