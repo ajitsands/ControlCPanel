2b1dadbc7db4037e084723f8dcfe40c32c720764
Ajit Kumar <ajitsands@gmail.com>
2024-05-12 00:25:13 +0530

    12-05-2024-12-25

--- class_control_cpanel.php
+++ class_control_cpanel.php
@@ -1,15 +1,17 @@
 <?php 
-
+include_once "settings.php";
 class ClassControlCpanel
 {
     
     public $cpanel_username,$cpanel_password,$subdomain,$domain,$directory;
     public function __construct() {
-        $this->cpanel_username = "sandsl23";
-        $this->cpanel_password = "d0mainsecur1ty@123!@#x%1";
-        $this->subdomain = 'newsubdomain';
-        $this->domain = 'sandslab.com';
-        $this->directory = "/public_html/".$this->subdomain;
+        $this->cpanel_username = CPANELUSERNAME ;
+        $this->cpanel_password = CPANELPASSWORD;
+        $this->subdomain = SUBDOMAIN;
+        $this->domain = DOMAIN;
+        $this->directory = DIRECTORY;
+
+        echo $this->directory;
     }
     
     
@@ -20,5 +22,7 @@ class ClassControlCpanel
     }
 
 }
+
+$ccp = new ClassControlCpanel();
 
 ?>
--- settings.php
+++ settings.php
@@ -2,5 +2,7 @@
     define('CPANELUSERNAME','sandsl23');
     define('CPANELPASSWORD','d0mainsecur1ty@123!@#x%1');
     define('DOMAIN','sandslab.com');
-    $subdomain = $_POST['subdomainname'];
+    define('SUBDOMAIN',$_POST['subdomainname']);
+    define('DIRECTORY','/public_html/'.SUBDOMAIN);
+   
 ?>
