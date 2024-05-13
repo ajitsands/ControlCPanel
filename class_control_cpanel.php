1725d16b11dd53f6a75a096166d7b2c3b85683f7
SaNDS Lab <ajitsands@gmail.com>
2024-05-13 13:28:58 +0530

    Update class_control_cpanel.php

--- class_control_cpanel.php
+++ class_control_cpanel.php
@@ -298,6 +298,7 @@ class ClassControlCpanel
 					'cpanel_jsonapi_user' => CPANELUSERNAME,
 					'cpanel_jsonapi_apiversion' => 2,
 					'cpanel_jsonapi_module' => 'cpanel',
+					'cpanel_jsonapi_func' => 'execute_query',
 					'cpanel_jsonapi_module2' => 'MysqlFE',
 					'cpanel_jsonapi_func2' => 'execute_query',
 					'arguments' => json_encode(array(
